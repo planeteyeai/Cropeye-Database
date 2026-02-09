05a8393c74b95d394852d4716f827eb50371f2a4
Internsplaneteyeinfra <interns@planeteyeinfra.ai>
2026-02-09 12:11:22 +0530

    Update shared_services.py

--- shared_services.py
+++ shared_services.py
@@ -66,7 +66,7 @@ class PlotSyncService:
     Service to fetch plot data from Django /plots/ API
     """
     
-    def __init__(self, django_api_url: str = "https://cropeye-server-flyio.onrender.com/admin/"):
+    def __init__(self, django_api_url: str = "https://cropeye-server-flyio.onrender.com"):
         self.django_api_url = django_api_url
         self.plots_cache = {}
         self.last_sync = None
