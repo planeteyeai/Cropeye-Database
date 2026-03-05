f093240382cffee04b032d52b39151b1e4a9caf0
Planeteyeai <tushar.patil@mitconindia.com>
2026-03-05 18:21:25 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,18 @@
-import os
-import psycopg2
+from supabase import create_client
 from dotenv import load_dotenv
-
+import os
+import json
+# Load .env file
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
-if not DATABASE_URL:
-    raise Exception("DATABASE_URL missing")
+if not SUPABASE_URL or not SUPABASE_SERVICE_ROLE_KEY:
+    raise Exception("Supabase env vars missing")
 
-def get_connection():
-    conn = psycopg2.connect(DATABASE_URL)
-    return conn
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE_KEY
+)
+
